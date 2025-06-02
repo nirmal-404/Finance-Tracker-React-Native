e78f32b3e9b468a1fd4e86284dbe5c6e5564a1cb
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-06-03 02:39:52 +0530

    authentication

---
+++ mobile/componensts/safe-screen.jsx
@@ -0,0 +1,19 @@
+import { View } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import {COLORS} from "@/constants/colors"
+
+const SafeScreen = ({ children }) => {
+    const insets = useSafeAreaInsets();
+    return (
+        <View style={{
+            paddingTop: insets.top,
+            paddingBottom: insets.bottom,
+            flex: 1,
+            backgroundColor: COLORS.background
+        }}>
+            {children}
+        </View>
+    )
+} 
+
+export default SafeScreen
